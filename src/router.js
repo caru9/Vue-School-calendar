c5154fdf0e6fb170f40c5423a99d5b72a7dc7f5f
caru9 <69743685+caru9@users.noreply.github.com>
2020-08-16 23:57:12 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Absence from './Absence'
 import Lesson from './Lesson.vue'
 import Cluster from './Cluster.vue'
 import Home from './Home.vue'
